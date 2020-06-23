2ad3a23ca80467bd673b91ecd5dfaab46c82cadc
ngoc nguyen <hapucoday@gmail.com>
2020-06-23 10:10:11 +0700

    Add find user by email and phone method

--- src/main/java/com/ecommerce/ecommerce/repositories/UserRepository.java
+++ src/main/java/com/ecommerce/ecommerce/repositories/UserRepository.java
@@ -10,4 +10,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     public Optional<User> findByUsername(String username);
+
+    public Optional<User> findByUserEmail(String userEmail);
+
+    public Optional<User> findByUserPhone(String userPhone);
 }
