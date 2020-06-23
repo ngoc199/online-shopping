95f3b85a0c46e5b5c335bb209e0d4cd6aab2af56
ngoc nguyen <hapucoday@gmail.com>
2020-06-23 10:44:06 +0700

    Add find methods to Address & AddressBook

--- src/main/java/com/ecommerce/ecommerce/repositories/AddressBookRepository.java
+++ src/main/java/com/ecommerce/ecommerce/repositories/AddressBookRepository.java
@@ -1,11 +1,12 @@
 package com.ecommerce.ecommerce.repositories;
 
 import com.ecommerce.ecommerce.entities.AddressBook;
+import com.ecommerce.ecommerce.entities.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressBookRepository extends JpaRepository<AddressBook, Long> {
-
+    public AddressBook findByUser(User user);
 }
--- src/main/java/com/ecommerce/ecommerce/repositories/AddressRepository.java
+++ src/main/java/com/ecommerce/ecommerce/repositories/AddressRepository.java
@@ -1,11 +1,14 @@
 package com.ecommerce.ecommerce.repositories;
 
+import java.util.List;
+
 import com.ecommerce.ecommerce.entities.Address;
+import com.ecommerce.ecommerce.entities.AddressBook;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
-
+    public List<Address> findByAddressBook(AddressBook addressBook);
 }
